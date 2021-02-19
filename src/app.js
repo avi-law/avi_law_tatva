711e308da7d4ba057e72f4c8f8661dcda6fb76ae
Praful Mali <praful.mali@tatvasoft.com>
2021-02-19 20:16:15 +0530

    ALSW-31 Send NL to Users according to their preferences

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const app = express();
 
+app.use(express.json({ limit: "50mb" }));
 server.applyMiddleware({ app });
 
 app.listen({ port: process.env.PORT || 4000 }, () =>
