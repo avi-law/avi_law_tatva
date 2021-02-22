51db79654dbb75916e245eb295fb2159c93c1394
Praful Mali <praful.mali@tatvasoft.com>
2021-02-22 15:44:19 +0530

    [CHANGES] -ALSW-30 NL-Area for Website Users

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { server } = require("./config");
+const cors = require("cors");
+const { server, corsConfig } = require("./config");
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json({ limit: "50mb" }));
+// app.use(cors(corsConfig));
 server.applyMiddleware({ app });
 
 app.listen({ port: process.env.PORT || 4000 }, () =>
--- src/config/application.js
+++ src/config/application.js
@@ -2,13 +2,28 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const corsConfig = {
+  origin: (origin, callback) => {
+    const arrayOfOrigin = process.env.CORS_ORIGIN
+      ? process.env.CORS_ORIGIN.split(",")
+      : [];
+    // if API call from same origin then the origin return undefined
+    if (origin === undefined || arrayOfOrigin.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS header origin cannot be added"));
+    }
+  },
+  exposedHeaders: ["X-Response-Time"],
+};
+
 module.exports = {
   // Application environment configuration
   port: process.env.PORT || 4000,
   environment: process.env.NODE_ENV || "development",
 
   defaultLanguage: process.env.DEFAULT_LANGUAGE || "en",
-
+  corsConfig,
   // JWT token for authentication
   jwtSecret: process.env.JWT_SECRET || "secret",
   jwtExpiresIn: process.env.JWT_EXPIRED_IN || "1d",
