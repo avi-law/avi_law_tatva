033ec918e03f53cb6f239d9dbab567ff298615ac
Praful Mali <praful.mali@tatvasoft.com>
2021-03-04 12:19:35 +0530

    ALSW-136 Post NL on Twitter

--- src/graphql/resolver/news-letters/tweet-news-letter.js
+++ src/graphql/resolver/news-letters/tweet-news-letter.js
@@ -1,11 +1,57 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable consistent-return */
 const driver = require("../../../config/db");
-const { APIError } = require("../../../utils");
+const { APIError, common } = require("../../../utils");
 const { defaultLanguage } = require("../../../config/application");
+const { getNewsletter, tweetNewsletter } = require("../../../neo4j/query");
 
+const getNewsLetterDetails = async (nlID) => {
+  const session = driver.session();
+  try {
+    const result = await session.run(getNewsletter, { nl_id: nlID });
+    if (result && result.records.length > 0) {
+      const newsLetters = result.records.map((record) => {
+        const nls = {
+          de: {
+            nl_text: null,
+            nl_title_long: null,
+            nl_title_short: null,
+          },
+          en: {
+            nl_text: null,
+            nl_title_long: null,
+            nl_title_short: null,
+          },
+        };
+        if (record.get("nls") && record.get("nls").length > 0) {
+          record.get("nls").forEach((nlState) => {
+            if (
+              nlState.lang &&
+              nlState.nls &&
+              nlState.lang.properties.iso_639_1
+            ) {
+              nls[nlState.lang.properties.iso_639_1] = nlState.nls.properties;
+            }
+          });
+        }
+        const nlResult = {
+          nl: common.getPropertiesFromRecord(record, "nl"),
+          nls,
+          country: common.getPropertiesFromRecord(record, "cou"),
+          user: common.getPropertiesFromRecord(record, "u"),
+        };
+        return nlResult;
+      });
+      session.close();
+      return newsLetters[0];
+    }
+  } catch (error) {
+    session.close();
+    return null;
+  }
+};
 
-module.exports = (object, params, ctx) => {
+module.exports = async (object, params, ctx) => {
   const { user } = ctx;
   const systemAdmin = user.user_is_sys_admin || null;
   const userSurfLang = user.user_surf_lang || defaultLanguage;
@@ -19,7 +65,25 @@ module.exports = (object, params, ctx) => {
         message: "INTERNAL_SERVER_ERROR",
       });
     }
-    return true;
+    const newsLetter = await getNewsLetterDetails(nlID);
+    if (newsLetter) {
+      if (newsLetter.nl.nl_tweeted) {
+        throw new APIError({
+          lang: userSurfLang,
+          message: "INTERNAL_SERVER_ERROR",
+        });
+      }
+      const result = await session.run(tweetNewsletter, { nl_id: nlID });
+      if (result && result.records.length > 0) {
+        session.close();
+        return true;
+      }
+      throw new APIError({
+        lang: defaultLanguage,
+        message: "INTERNAL_SERVER_ERROR",
+      });
+    }
+    return false;
   } catch (error) {
     session.close();
     throw error;
