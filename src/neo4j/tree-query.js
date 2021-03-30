a04ea2f54bb71202767b3c58bd8b4a7374cfe34f
Praful Mali <praful.mali@tatvasoft.com>
2021-03-30 17:36:48 +0530

    ALSW-172 Sol -BE - remove gray bar

--- src/neo4j/tree-query.js
+++ src/neo4j/tree-query.js
@@ -50,3 +50,41 @@ CALL {
 }
 RETURN res_rbss {.rule_book_struct_id, .rule_book_struct_active, rule_book_struct_parent_id: res_rbss_parent.rule_book_struct_id, res_desc_lang, label: labels(res_rbss)} AS rbss_res, rbs_res
 `;
+
+exports.getRuleBookTreeStructure = `
+MATCH (rb:Rule_Book {rule_book_id: $rule_book_id })
+WITH rb, rb {.rule_book_id, .rule_book_active} AS rb2
+CALL {
+      WITH rb
+      CALL apoc.path.expandConfig(rb, {relationshipFilter: "HAS_RULE_BOOK_CHILD>"})
+      YIELD path
+
+CALL {
+WITH path
+WITH nodes(path)[-1] as rb
+MATCH (rb)-[:HAS_RULE_BOOK_ISSUE]->(rbi:Rule_Book_Issue)
+WITH rbi ORDER BY rbi.rule_book_issue_no
+CALL {
+WITH rbi
+MATCH (rbi)-[:HAS_RULE_BOOK_ISSUE_STATE]->(rbis:Rule_Book_Issue_State)-[:RULE_BOOK_ISSUE_LANGUAGE_IS]->(lang:Language)
+RETURN rbis, lang
+}
+RETURN collect(rbi {.rule_book_issue_no, title_short: rbis.rule_book_issue_title_short, title_long: rbis.rule_book_issue_title_long, language: lang.iso_639_1, label: labels(rbi)}) AS res_rbi
+}
+
+WITH path, nodes(path) AS n, res_rbi, apoc.text.rpad(reduce(a = "", r in relationships(path) | a + apoc.text.lpad(toString(COALESCE(r.order_rule_book_child,0)),4,"0")),100,"0") AS orders
+    RETURN n[-1] AS res_rbs, n[-2] AS res_rbs_parent, res_rbi ORDER BY orders
+}
+RETURN collect(res_rbs {.rule_book_id, .rule_book_active, rule_book_parent_id: res_rbs_parent.rule_book_id, res_rbi, label: labels(res_rbs)}) AS rbs_res
+`;
+
+exports.getRuleBookIssueTreeStructure = `
+MATCH (Rule_Book {rule_book_id: $rule_book_id)-[:HAS_RULE_BOOK_ISSUE]->(rbi:Rule_Book_Issue)
+WITH rbi ORDER BY rbi.rule_book_issue_no
+CALL {
+WITH rbi
+MATCH (rbi)-[:HAS_RULE_BOOK_ISSUE_STATE]->(rbis:Rule_Book_Issue_State)-[:RULE_BOOK_ISSUE_LANGUAGE_IS]->(lang:Language)
+RETURN rbis, lang
+}
+RETURN collect(rbi {.rule_book_issue_no, title_short: rbis.rule_book_issue_title_short, title_long: rbis.rule_book_issue_title_long, language: lang.iso_639_1, label: labels(rbi)}) AS res_rbi
+`;
