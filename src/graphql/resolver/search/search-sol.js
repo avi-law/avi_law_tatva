dc1484141b1e347d2434a168d431ba54235f3748
Praful Mali <praful.mali@tatvasoft.com>
2021-04-21 12:59:10 +0530

    ALSW-183 Provisions - BE - Tree structure and detail data should be independent

--- src/graphql/resolver/search/search-sol.js
+++ src/graphql/resolver/search/search-sol.js
@@ -1,4 +1,5 @@
 /* eslint-disable consistent-return */
+const _ = require("lodash");
 const driver = require("../../../config/db");
 const { common, constants } = require("../../../utils");
 const {
@@ -146,7 +147,7 @@ module.exports = async (object, params, ctx) => {
           ...common.getPropertiesFromRecord(record, "sl"),
           sol_state: sls,
           languageDisplay:
-            languages.length > 0 ? languages.sort().join(" / ") : "-",
+            languages.length > 0 ? _.uniq(languages).sort().join(" / ") : "-",
         };
         return slResult;
       });
