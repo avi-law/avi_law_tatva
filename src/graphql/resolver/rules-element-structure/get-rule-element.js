0e374aa789487bfeb26139f1a74e2f1d7d35dd72
Praful Mali <praful.mali@tatvasoft.com>
2021-04-27 17:54:05 +0530

    ALSW-186 Provisions - BE - CRUD Rule-Elements

--- src/graphql/resolver/rules-element-structure/get-rule-element.js
+++ src/graphql/resolver/rules-element-structure/get-rule-element.js
@@ -40,6 +40,11 @@ module.exports = async (object, params, ctx) => {
             ) {
               const properties = _.get(reState, "res.properties", null);
               if (properties) {
+                if (properties.rule_elemnet_title !== "") {
+                  properties.rule_element_title = common.removeTag(
+                    properties.rule_element_title
+                  );
+                }
                 if (!res[properties.rule_element_id]) {
                   res[properties.rule_element_id] = {};
                 }
