a7325bf70b4e01e7692e7913d73489dba5a904f3
Praful Mali <praful.mali@tatvasoft.com>
2021-07-07 18:33:38 +0530

    ALSW-233 Provisions - FE - Display back linked provisions

--- src/neo4j/user-query.js
+++ src/neo4j/user-query.js
@@ -10,7 +10,7 @@ WITH collect({ log: properties(log3), lt: properties(lt), data: obj }) as logs
 RETURN logs
 `;
 exports.getUserHistoryLogs = `
-MATCH (log:Log)-[:LOG_FOR_USER]->(u:User {user_email: "janezic@avi-law.com"})
+MATCH (log:Log)-[:LOG_FOR_USER]->(u:User {user_email: $user_email})
 WHERE NOT (log)<-[:USER_LOG_PREDECESSOR]-(:Log)
 WITH log
 MATCH (log:Log)-[:USER_LOG_PREDECESSOR*0..19]->(log3:Log)-[:LOG_REFERS_TO_OBJECT]-(obj)
