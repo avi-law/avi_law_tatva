d6f6e1e9b9edf959c88f5a8eb95f706f7283c912
Praful Mali <praful.mali@tatvasoft.com>
2021-07-08 15:18:36 +0530

    ALSW-236 - User-History

--- src/graphql/resolver/user/get-user-history-log.js
+++ src/graphql/resolver/user/get-user-history-log.js
@@ -2,74 +2,12 @@
 /* eslint-disable consistent-return */
 const _ = require("lodash");
 const driver = require("../../../config/db");
-const { APIError, constants, common } = require("../../../utils");
-const { getUserHistoryLogs } = require("../../../neo4j/user-query");
+const { APIError } = require("../../../utils");
 const {
-  getLogRefersToRuleElementStateDetails,
-} = require("../../../neo4j/rule-element-query");
+  getUserHistoryLogs,
+  getUserHistoryLogsCount,
+} = require("../../../neo4j/user-query");
 const { defaultLanguage } = require("../../../config/application");
-
-const getRuleElementStateData = async (ruleElementState) => {
-  const session = driver.session();
-  try {
-    const identity = _.get(ruleElementState, "identity", null);
-    if (identity) {
-      const ruleStatResultDetails = await session.run(
-        getLogRefersToRuleElementStateDetails,
-        {
-          array_of_identity: [identity],
-        }
-      );
-      const stateObject = {
-        re: null,
-        rbis: null,
-        res: {
-          en: null,
-          de: null,
-        },
-      };
-      if (ruleStatResultDetails && ruleStatResultDetails.records.length > 0) {
-        ruleStatResultDetails.records.forEach((record) => {
-          const stateIdentity = record.get("res1").identity || null;
-          const res1 = common.getPropertiesFromRecord(record, "res1");
-          const lang1 = common.getPropertiesFromRecord(record, "lang1");
-          const res2 = common.getPropertiesFromRecord(record, "res2");
-          const lang2 = common.getPropertiesFromRecord(record, "lang2");
-          stateObject.rbis = record.get("rbis");
-          stateObject.re = common.getPropertiesFromRecord(record, "re");
-          if (res1 && lang1) {
-            stateObject.res[lang1.iso_639_1] = res1;
-            stateObject.res[lang1.iso_639_1].identity = stateIdentity;
-          }
-          if (res2 && lang2) {
-            stateObject.res[lang2.iso_639_1] = res2;
-            stateObject.res[lang2.iso_639_1].identity = stateIdentity;
-          }
-        });
-      }
-      return stateObject;
-    }
-    return null;
-  } catch (error) {
-    session.close();
-    throw error;
-  } finally {
-    session.close();
-  }
-};
-
-const getLogData = (logs) => {
-  let data = null;
-  const logTypeId = _.get(logs, "lt.log_type_id", null);
-  switch (logTypeId) {
-    case constants.LOG_TYPE_ID.READ_RULE_ELEMENT_AND_STATE:
-      data = getRuleElementStateData(logs.data);
-      break;
-    default:
-      break;
-  }
-  return data;
-};
 
 module.exports = async (object, params, ctx) => {
   params = JSON.parse(JSON.stringify(params));
@@ -78,12 +16,20 @@ module.exports = async (object, params, ctx) => {
   const userEmail = user.user_email || null;
   const session = driver.session();
   let logData = null;
+  let total = 0;
   try {
     if (!userEmail) {
       throw new APIError({
         lang: userSurfLang,
         message: "INTERNAL_SERVER_ERROR",
       });
+    }
+    const countResult = await session.run(getUserHistoryLogsCount, {
+      user_email: userEmail,
+    });
+    if (countResult && countResult.records.length > 0) {
+      const singleRecord = countResult.records[0];
+      total = singleRecord.get("count");
     }
     const result = await session.run(getUserHistoryLogs, {
       user_email: userEmail,
@@ -115,6 +61,8 @@ module.exports = async (object, params, ctx) => {
     //   const lData = await getLogData(logData);
     //   logData.data = JSON.stringify(lData);
     // }
+    logData.total = total;
+    console.log(logData);
     return logData;
   } catch (error) {
     session.close();
--- src/neo4j/user-query.js
+++ src/neo4j/user-query.js
@@ -1,7 +1,7 @@
 const { constants } = require("../utils");
 
 exports.getUserHistoryLogs = `
-MATCH (lt:Log_Type)<-[:HAS_LOG_TYPE]-(log:Log)-[:LOG_FOR_USER]->(u:User {user_email: $user_email}), (log:Log)-[:LOG_REFERS_TO_OBJECT]-(obj)
+MATCH (lt:Log_Type)<-[:HAS_LOG_TYPE]-(log:Log)-[:LOG_FOR_USER]->(u:User {user_email: $user_email}), (log)-[:LOG_REFERS_TO_OBJECT]-(obj)
 WHERE lt.log_type_id IN [${constants.LOG_TYPE_ID.READ_RULE_BOOK},${constants.LOG_TYPE_ID.READ_RULE_ELEMENT_AND_STATE},${constants.LOG_TYPE_ID.READ_NL}]
 WITH *, collect(obj) as obj
 WITH *,  apoc.coll.toSet(obj) as obj ORDER BY log.log_timestamp DESC LIMIT 20
@@ -24,7 +24,7 @@ CALL apoc.do.case([
     WITH *, re, path, MIN(length(path)) as minLength ORDER BY minLength ASC LIMIT 1
     WITH *, nodes(path)[minLength] as rbi
     MATCH (rb:Rule_Book)-[:HAS_RULE_BOOK_ISSUE]->(rbi)-[:HAS_RULE_BOOK_ISSUE_STATE]->(rbis:Rule_Book_Issue_State)-[:RULE_BOOK_ISSUE_LANGUAGE_IS]->(lang:Language)
-    WITH collect({ identity: id(obj[0]), rule_element_title: obj[0].rule_element_title, rule_element_article: obj[0].rule_element_article, rule_element_doc_id: re.rule_element_doc_id, rule_book_issue_title_short: rbis.rule_book_issue_title_short, iso_639_1: lang.iso_639_1}) as data
+    WITH collect({ identity: id(res), rule_element_title: res.rule_element_title, rule_element_article: res.rule_element_article, rule_element_doc_id: re.rule_element_doc_id, rule_book_issue_title_short: rbis.rule_book_issue_title_short, iso_639_1: lang.iso_639_1}) as data
     RETURN data',
   lt.log_type_id = ${constants.LOG_TYPE_ID.READ_RULE_BOOK} AND LABELS(obj[0])[0] = "${constants.LOG_REFERS_TO_OBJECT_LABEL.RULE_BOOK}",
     'MATCH (rb:Rule_Book { rule_book_id: obj[0].rule_book_id})-[:HAS_RULE_BOOK_ISSUE]->(rbi:Rule_Book_Issue)-[:HAS_RULE_BOOK_ISSUE_STATE]->(rbis:Rule_Book_Issue_State)-[:RULE_BOOK_ISSUE_LANGUAGE_IS]->(lang:Language)
@@ -41,4 +41,11 @@ CALL apoc.do.case([
 YIELD value
 WITH collect({ log: properties(log), lt: properties(lt), data: value.data }) as logs
 RETURN logs
+`;
+exports.getUserHistoryLogsCount = `
+MATCH (lt:Log_Type)<-[:HAS_LOG_TYPE]-(log:Log)-[:LOG_FOR_USER]->(u:User {user_email: $user_email}), (log)-[:LOG_REFERS_TO_OBJECT]-(obj)
+WHERE lt.log_type_id IN [${constants.LOG_TYPE_ID.READ_RULE_BOOK},${constants.LOG_TYPE_ID.READ_RULE_ELEMENT_AND_STATE},${constants.LOG_TYPE_ID.READ_NL}]
+WITH *, collect(obj) as obj
+WITH *,  apoc.coll.toSet(obj) as obj ORDER BY log.log_timestamp DESC
+RETURN count(log) as count
 `;
